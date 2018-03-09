35ce7c2d702fd5ae472e9710c7282db6a2975adb
Cameron Sharp <cameron.royal.sharp@gmail.com>
2018-03-09 09:34:29 -0700

    Initial implementation of Bootstrap progress bars and JS to animate them

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var skills = document.getElementsByClassName('skills-content')
+var skillBars = document.getElementsByClassName('progress-bar')
+
+for (var i = 0; i < skills.length; i++) {
+    skills[i].addEventListener("mouseover", setProgressBar)
+}
+
+for (var i = 0; i < skills.length; i++) {
+    skills[i].addEventListener("mouseleave", resetProgressBars)
+}
+
+function setProgressBar(e) {
+    var id = e.target.getAttribute('id')
+    var progressBarDiv = document.getElementById(`${id}-bar`)
+    // var barDiv = document.querySelector(`#${id} > .skills-bar`)
+    if (progressBarDiv) {
+        var barMaxValue = progressBarDiv.getAttribute('aria-valuemax')
+        progressBarDiv.setAttribute("style", `width: ${barMaxValue}%`)
+    }
+}
+
+function resetProgressBars() {
+    console.log("derp")
+    for (var i = 0; i < skillBars.length; i++) {
+        skillBars[i].setAttribute("style", `width: 0%`)
+    }
+}
